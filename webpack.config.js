5082cd58f072a8cf60fe9cdf5558b8b218ae517b
ArchiAngels <mikita.valynchyts@gmail.com>
2021-07-28 23:49:03 +0200

    free

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    mode:"development",
-    // mode:'production',
+    // mode:"development",
+    mode:'production',
     entry: './src/scripts/myReactApp.jsx',
     output: {
         filename: 'bundled.myReactApp.js',
